dca8a61a274f19840a0114c2ba35a9dc6f694202
Mohammed Brahimi <45235641+Tahedi1@users.noreply.github.com>
2026-03-11 05:58:05 +0100

    Update next.config.ts
    change the basePath and the asset Prefix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/agri-challenge2",
-  assetPrefix: "/agri-challenge2",
+  basePath: "/edition-2024",
+  assetPrefix: "/edition-2024",
   images: { unoptimized: true },
 };
 
